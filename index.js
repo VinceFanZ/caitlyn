2c1eb2ba46001804e7d2a2b7d7bbb590c5a062cb
vince <vincefanz@gmail.com>
2017-08-22 22:21:24 +0800

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule')
 const signIn = require('./src/sign-in.js')
 
 const rule = new schedule.RecurrenceRule()
-rule.hour = 12
+rule.hour = 6
 
 const j = schedule.scheduleJob(rule, () => {
   signIn()
